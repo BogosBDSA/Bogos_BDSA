5419a411c666c2481959ca9ffe2b7769eb468da4
nsel <nsel@itu.dk>
2022-10-26 09:25:27 +0200

    adding stuff

--- Bogos.tests/UnitTest1.cs
+++ Bogos.tests/UnitTest1.cs
@@ -2,14 +2,21 @@ namespace Bogos.tests;
 
 public class UnitTest1
 {
-    
-
     [Fact]
     public void testThatTestingWorks()
     {  
     var test1 = new Onlytest();
 
-    test1.testmethod("it").Equals("it works"); 
-    
+    test1.testmethod("it").Equals("it works");    
+    }
+}
+
+public class gitInsight
+{
+    [Fact]
+    public void Test_Number_Of_Commits_Per_Day()
+    {
+        //this test only works on the 25/10/2022
+        
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 ## welcome to bogos
 
-if your program dosent work do a dotnet build first .
+fluentassertions and libgit2sharp have to be added 
+libgit is needed in both test and program, fluentassertions is only needed in tests
+
+### import them using 
+important to do in the correct folders
+```
+dotnet add package LibGit2Sharp --version 0.26.2
+dotnet add package FluentAssertions --version 6.8.0
+```
