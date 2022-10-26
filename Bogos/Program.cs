d142a690dfca86f16e5018855e792a86ebf59643
nsel <nsel@itu.dk>
2022-10-26 18:06:34 +0200

    added algorithm for frequency mode

--- Bogos/Program.cs
+++ Bogos/Program.cs
@@ -1,4 +1,5 @@
-﻿using LibGit2Sharp;
+﻿using System.Collections;
+using LibGit2Sharp;
 
 namespace Bogos;
 
@@ -7,11 +8,32 @@ namespace Bogos;
         public static void Main(string[] args)
       {
           using (var repo = new Repository(@"../"))
+          
           {
+            var dates = new ArrayList(); 
             foreach (Commit commit in repo.Commits)
             {
+              /*
             Console.WriteLine("Author: {0}", commit.Author.Name);
             Console.WriteLine("Message: {0}", commit.MessageShort);
+            Console.WriteLine("date: {0}", commit.Author.When);
+            */
+            dates.Add(commit.Author.When.Date.ToString());
+            }
+            var counter = 1; 
+            var dict = new Dictionary<string, int>(); 
+            foreach (string date in dates)
+            {
+            if(dict.ContainsKey(date))
+            {
+            dict[date]=dict[date]+1;
+            } else {
+              dict.Add(date, counter); 
+            } 
+            }
+            foreach (var number in dict)
+            {
+              Console.WriteLine(number); 
             }
           }
       }
