450da6932c99e0ce231ecf9f07db0203cf11eccc
SDeLaVida <paab@itu.dk>
2022-10-28 15:11:24 +0200

    made helper class to test

--- Bogos.tests/GitAuthorModeTests.cs
+++ Bogos.tests/GitAuthorModeTests.cs
@@ -1,3 +1,5 @@
+using Bogos.tests;
+
 public class gitInsightTests : IDisposable
 {
     DateTimeOffset time1 = new DateTimeOffset(new DateTime(2020, 05, 05));
@@ -18,19 +20,5 @@ public class gitInsightTests : IDisposable
         //used to delete repo when we're done
         testRepo.Dispose();
         DeleteReadOnlyDirectory(repoPath);
-    }
-    public static void DeleteReadOnlyDirectory(string directory)
-    {
-        foreach (var subdirectory in Directory.EnumerateDirectories(directory))
-        {
-            DeleteReadOnlyDirectory(subdirectory);
-        }
-        foreach (var fileName in Directory.EnumerateFiles(directory))
-        {
-            var fileInfo = new FileInfo(fileName);
-            fileInfo.Attributes = FileAttributes.Normal;
-            fileInfo.Delete();
-        }
-        Directory.Delete(directory);
     }
 }
