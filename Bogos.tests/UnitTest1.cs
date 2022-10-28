08e658849d4b7fc043612815edc539590387e54a
nsel <nsel@itu.dk>
2022-10-28 14:26:51 +0200

    changes

--- Bogos.tests/UnitTest1.cs
+++ Bogos.tests/UnitTest1.cs
@@ -19,14 +19,12 @@ public class gitInsightTests : IDisposable
     String repoPath;
     public gitInsightTests() 
     {
-        try{
-    repoPath = Repository.Init(@"../../.././testRepo");
+    repoPath = Repository.Init(@"./testRepo");
     testRepo = new Repository(repoPath);
     var options = new CommitOptions();
     options.AllowEmptyCommit=true;
     var signature1 = new Signature("Nicolai", "bøvmail@123", time1);
     testRepo.Commit("inital commit",signature1,signature1,options);  
-        } finally {Dispose();}
     }
 
     [Fact]
@@ -38,16 +36,31 @@ public class gitInsightTests : IDisposable
 
         //act 
         var repo = new frequency(path);
+        
         var results = repo.gitInsightfrequency(); 
-
+        Dispose();
         //assert
         results.Should().BeEquivalentTo(expected);
+        
     }
 
     public void Dispose()
     {
         //used to delete repo when we're done
         testRepo.Dispose();
-        Directory.Delete(repoPath,true);
+        DeleteDirectory(repoPath);
+    }
+     private static void DeleteDirectory(string directory)
+    {
+        foreach (string fileName in Directory.EnumerateFiles(directory))
+        {
+            var fileInfo = new FileInfo(fileName)
+            {
+                Attributes = FileAttributes.Normal
+            };
+            fileInfo.Delete();
+        }
+
+        Directory.Delete(directory);
     }
 }
