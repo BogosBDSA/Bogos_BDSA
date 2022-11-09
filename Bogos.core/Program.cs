5478cd061c1ec22449a2841c334b3fd997872029
SDeLaVida <paab@itu.dk>
2022-11-09 21:05:27 +0100

    refactor authormode and frequency mode

--- Bogos.core/Program.cs
+++ Bogos.core/Program.cs
@@ -4,7 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Repository repo;
         var builder = new DbContextOptionsBuilder<GitContext>().UseNpgsql(@"Server=127.0.0.1;Port=5430;Database=bogosdb;User Id=postgres;Password=mypassword;");
         using var db = new GitContext(builder.Options);
         db.Database.EnsureDeleted();
@@ -12,64 +11,22 @@ public class Program
 
 
 
-        while (true)
-        {
-            Console.WriteLine("Please enter a valid repository path.");
-            var repoPath = Console.ReadLine();
-            try
-            {
-                repo = new Repository(repoPath);
+        // API
 
-                if (repo != null) break;
-            }
-            catch (RepositoryNotFoundException)
-            {
-
-                Console.WriteLine("Not a valid path! Try again!\n");
-            }
-
-        }
-
-        while (true)
-        {
-            Console.WriteLine("\nPlease choose which mode you wish to use.");
-            Console.WriteLine("Type 1 for Frequency Mode");
-            Console.WriteLine("Type 2 for Author Mode");
+        // -> URI("")
+        // repo = GITREPO(URI) 
 
 
-            var mod = Console.ReadLine();
-            switch (mod)
-            {
-                case "1":
-                    Console.WriteLine("\nFrequency Mode:");
-                    var rep = new frequency(repo);
-                    var res = rep.gitInsightfrequency();
-                    int Id = 0;
-                    /*  foreach (var commit in res)
-                     {
-                         Id++
-                         db.commits.Append(new GitCommit(commit, repo));
-                         db.SaveChanges();
-                     }
-                     foreach (var FrequencyData in db.FD)
-                     {
+        // AUTHOR MODE <- repo
 
-                         Console.Write(FrequencyData.Commit);
-                         Console.WriteLine(FrequencyData.Date);
-                     } */
+        // FREQUENCY MODE <- repo
 
-                    break;
-                case "2":
-                    Console.WriteLine("\nAuthor Mode:");
-                    _ = new AuthorMode(repo);
-                    break;
-                default:
-                    Console.WriteLine("Unable to understand input. Try again!\n");
-                    continue;
-            }
-            break;
 
-        }
+
+
+
+
+
 
 
     }
--- Bogos.entities/GitRepo.cs
+++ Bogos.entities/GitRepo.cs
@@ -12,9 +12,11 @@ public class GitRepo
     public GitRepo()
     {
     }
-    public GitRepo(Repository repo, string uri, string branchName = "")
+    public GitRepo(string uri, string branchName = "")
     {
+        // FUNKTIONALITET med at ødelægge det
 
+        var repo = new Repository();
         Commits = new List<GitCommit>();
         if (branchName != "" && repo.Branches[branchName] != null) { Commands.Checkout(repo, branchName); }
 
