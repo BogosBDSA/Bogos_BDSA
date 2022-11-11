05b22097846b67dfcc50a8ebada0d53ecb252484
nsel <nsel@itu.dk>
2022-11-11 14:24:13 +0100

    fixed gitrepo

--- Bogos.entities/GitRepo.cs
+++ Bogos.entities/GitRepo.cs
@@ -3,10 +3,6 @@ public class GitRepo
 {
     public int Id { get; set; }
 
-    // skal måske ændres til url, når vi arbejder med vores API, dette giver dog ingen mening
-    // hvis man bruger programmet lokalt, da man vil give det en system path.
-
-    // Måske bruge første commit til at samligne.
     public Uri? Uri { get; set; }
     public virtual ICollection<GitCommit> Commits { get; set; } = new List<GitCommit>();
     public GitRepo()
@@ -14,32 +10,26 @@ public class GitRepo
     }
     public GitRepo(string url, string branchName = "")
     {
+        Repository repo = null;
         var tempPath = "./TempRepoFolder";
         try
         {
-            Directory.CreateDirectory(tempPath);
-            var opt = new CloneOptions();
-            var clonedPath = Repository.Clone(url, tempPath, opt);
-            var repo = new Repository(clonedPath);
+            var clonedPath = Repository.Clone(url, tempPath);
+            repo = new Repository(clonedPath);
             if (branchName != "" && repo.Branches[branchName] != null) { Commands.Checkout(repo, branchName); }
             Commits = repo.Commits.Select(commit => new GitCommit(this, commit)).ToList();
-            // Kan muligvis fjernes, kommer an på hvordan det sorteres.
-            this.Commits.ToList().Sort((a, b) => a.CommitterWhen.CompareTo(b.CommitterWhen));
         }
         finally
-        {
-
-            DeleteReadOnlyDirectory(tempPath);
-            Directory.Delete("TempRepoFolder");
-
+        {  
+            if(repo != null) {repo.Dispose();}
+            DeleteReadOnlyDirectory(tempPath);   
         }
-
-
     }
 
+
+//This function is borrowed from https://stackoverflow.com/questions/2316308/remove-readonly-attribute-from-directory
     private static void DeleteReadOnlyDirectory(string directory)
     {
-
         foreach (var subdirectory in Directory.EnumerateDirectories(directory))
         {
             DeleteReadOnlyDirectory(subdirectory);
