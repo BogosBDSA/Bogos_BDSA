1459ac9ef94aa3a200a21dfc8835ba4462f027a5
Oscar <oska@itu.dk>
2022-11-12 22:22:47 +0100

    Json convertion

--- Bogos.core/Program.cs
+++ Bogos.core/Program.cs
@@ -9,9 +9,14 @@ public class Program
         db.Database.EnsureDeleted();
         db.Database.EnsureCreated(); 
 
+        var build = WebApplication.CreateBuilder(args);
+
+        build.Services.AddDbContext<GitContext>(opt => opt = builder);
+
         var hardcodepath = "https://github.com/nselpriv/Bogos_BDSA.git"; 
         GitRepoRepository repository = new GitRepoRepository(db);
         var printable = repository.ReadRepoByUri(hardcodepath);
+        
         GitRepo printable2 = null;
         if(printable == null)
         {
@@ -37,7 +42,46 @@ public class Program
         var auth = GitInsight.AuthorMode(printable); 
         */
 
+        //opt.UseNpgsql(@"Server=127.0.0.1;Port=5430;Database=bogosdb;User Id=postgres;Password=mypassword;"));
 
+        build.Services.AddEndpointsApiExplorer();
+        build.Services.AddSwaggerGen();
+
+	    var app = build.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+	    app.MapGet("/", () => "Hello World!");
+
+    //Get function to Get data from database
+        //app.MapGet("/data", (GitContext data) => data.Repos.ToListAsync());
+
+        app.MapGet("/gitinsight/frequency/{repo}", (string repo) => {
+                var path = $"https://github.com/{repo}.git";
+                
+
+                var result = repository.ReadRepoByUri(hardcodepath);
+               
+                var converthis = GitInsight.FrequencyMode(result);
+                var returnthis = Newtonsoft.Json.JsonConvert.SerializeObject(converthis);
+                
+                
+                return returnthis;
+        });
+
+
+        app.MapGet("/gitinsight/author/{repo}", (string repo) => {
+                var path = $"https://github.com/{repo}.git";
+
+                var result = repository.ReadRepoByUri(hardcodepath);
+               
+                var converthis = GitInsight.AuthorMode(result);
+                var returnthis = Newtonsoft.Json.JsonConvert.SerializeObject(converthis);
+                
+                
+                return returnthis;
+        });
         // API
 
         // -> URI("")
@@ -48,7 +92,7 @@ public class Program
 
         // FREQUENCY MODE <- repo
 
-
+        app.Run();
 
 
 
