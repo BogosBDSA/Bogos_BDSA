100900898e9765d61ef01199baf76c95ca5bc034
Patrick N <paab@itu.dk>
2022-11-25 13:19:38 +0100

    fixed cors policies

--- Bogos.core/Program.cs
+++ Bogos.core/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Bogos.core;
+﻿
+namespace Bogos.core;
 using Bogos.entities;
 using Newtonsoft.Json;
 
@@ -10,7 +11,7 @@ public class Program
         var dbBuilder = new DbContextOptionsBuilder<GitContext>().UseNpgsql(@"Server=127.0.0.1;Port=5430;Database=bogosdb;User Id=postgres;Password=mypassword;");
         using var db = new GitContext(dbBuilder.Options);
         db.Database.EnsureDeleted();
-        db.Database.EnsureCreated(); 
+        db.Database.EnsureCreated();
 
         // Setup repository
         GitRepoRepository _repository = new GitRepoRepository(db);
@@ -25,38 +26,40 @@ public class Program
             options.AddDefaultPolicy(
                 policy =>
                 {
-                    policy.WithOrigins("http://localhost:5105");
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
                 });
         });
-	    var webApp = webAppBuilder.Build();
+        var webApp = webAppBuilder.Build();
         webApp.UseCors();
         webApp.UseSwagger();
         webApp.UseSwaggerUI();
 
         // Setup endpoints
-	    webApp.MapGet("/", () => 
+        webApp.MapGet("/", () =>
             @"To run Author and Frequency mode: 
             Type /frequency or /author your localHost:Port, 
             and add the users name and which repository you want to display. \n 
             For example : http://localhost:5243/frequency/<User>/<Repository>"
         );
-        webApp.MapGet("/frequency/{author}/{repo}", (string author, string repo) => {
-                var (_, gitRepo) = _repository.HandleUri($"https://github.com/{author}/{repo}.git");
-                if (gitRepo == null) 
-                {
-                    return "Error"; // TODO: Handle error
-                }
-                var frequencyModeDto = GitInsight.FrequencyMode(gitRepo);
-                return JsonConvert.SerializeObject(frequencyModeDto, Formatting.Indented);
+        webApp.MapGet("/frequency/{author}/{repo}", (string author, string repo) =>
+        {
+            var (_, gitRepo) = _repository.HandleUri($"https://github.com/{author}/{repo}.git");
+            if (gitRepo == null)
+            {
+                return "Error"; // TODO: Handle error
+            }
+            var frequencyModeDto = GitInsight.FrequencyMode(gitRepo);
+            return JsonConvert.SerializeObject(frequencyModeDto, Formatting.Indented);
         });
-        webApp.MapGet("/author/{author}/{repo}", (string author, string repo) => {
-                var (_, gitRepo) = _repository.HandleUri($"https://github.com/{author}/{repo}.git");
-                if (gitRepo == null) 
-                {
-                    return "Error"; // TODO: Handle error
-                }
-                var authorModeDto = GitInsight.AuthorMode(gitRepo);
-                return JsonConvert.SerializeObject(authorModeDto, Formatting.Indented);
+        webApp.MapGet("/author/{author}/{repo}", (string author, string repo) =>
+        {
+            var (_, gitRepo) = _repository.HandleUri($"https://github.com/{author}/{repo}.git");
+            if (gitRepo == null)
+            {
+                return "Error"; // TODO: Handle error
+            }
+            var authorModeDto = GitInsight.AuthorMode(gitRepo);
+            return JsonConvert.SerializeObject(authorModeDto, Formatting.Indented);
         });
 
         webApp.Run();
